b2a64f8786fc8ec2365e6f45eb95aa5e1d4db839
joshatxantie <josh@xantie.com>
2021-02-06 18:46:58 -0700

    Added Usability for Peer Review

---
+++ lesson-7/assets/js/weatherapi.js
@@ -0,0 +1,18 @@
+const KEY = '184a8d356da032cd8d665a8aed33b006';
+const URL = `http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=${KEY}`;
+const DOM = {} || DOM;
+DOM.currentTemp = document.getElementById('current-temp');
+
+
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    DOM.currentTemp.textContent = jsObject.main.temp
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+  });
